f3ee6a6645d11ce39792bfa36ac9294770f1c860
Jackop05 <jakubnyrbotzs@gmail.com>
2024-09-21 13:09:55 +0200

    More API endpoints working

--- JavaEventsApp_FrontEnd/src/pages/Main.jsx
+++ JavaEventsApp_FrontEnd/src/pages/Main.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
   const navigate = useNavigate();
-  
+
+  const [refresher, setRefresher] = useState(false);  
   const [loading, setLoading] = useState(false);
   const [newEventActive, setNewEventActive] = useState(false);
   const [eventData, setEventData] = useState({
@@ -32,7 +33,6 @@ const Main = () => {
           const userData = await response.json();
           setUser(userData);
           
-          // Fetch all events
           const eventsResponse = await fetch('http://localhost:9090/api/allEvents', {
             method: 'GET',
             headers: {
@@ -44,11 +44,8 @@ const Main = () => {
           if (eventsResponse.ok) {
             const allEvents = await eventsResponse.json();
             setEvents(allEvents);
-          } else {
-            console.error('Failed to fetch all events');
           }
         } else {
-          console.error('Failed to fetch user data');
           navigate('/login');
         }
       } catch (error) {
@@ -60,16 +57,41 @@ const Main = () => {
     };
 
     fetchUserData();
-  }, [navigate]);
+  }, [navigate, refresher]);
+
+  const toggleParticipation = async (eventId) => {
+    if (!user.name) return;
+
+    const isParticipating = user.interestedInEvents?.includes(eventId);
+    const apiEndpoint = isParticipating ? 'removeEvent' : 'addEvent';
+
+    try {
+      const response = await fetch(`http://localhost:9090/api/${apiEndpoint}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ eventId, name: user.name }),
+        credentials: 'include',
+      });
+
+      if (response.ok) {
+        const updatedUser = { ...user };
+        if (isParticipating) {
+          updatedUser.interestedInEvents = updatedUser.interestedInEvents.filter(id => id !== eventId);
+        } else {
+          updatedUser.interestedInEvents.push(eventId);
+        }
+        setUser(updatedUser);
+      }
+    } catch (error) {
+      console.error('Error toggling participation:', error);
+    }
+  };
 
   const createEvent = async () => {
     setLoading(true);
-    const eventRequest = {
-      name: eventData.name,
-      description: eventData.description,
-      location: eventData.location,
-      date: eventData.date,
-    };
+    const eventRequest = { ...eventData };
 
     try {
       const response = await fetch('http://localhost:9090/api/event', {
@@ -83,11 +105,8 @@ const Main = () => {
       setLoading(false);
       if (response.ok) {
         const newEvent = await response.json();
-        setEvents((prevEvents) => [...prevEvents, newEvent]); // Add the new event to the list
-        navigate('/newEvent');
-        navigate('/');
-      } else {
-        console.error('Failed to create event');
+        setEvents((prevEvents) => [...prevEvents, newEvent]);
+        setRefresher(!refresher);
       }
     } catch (error) {
       console.error('Error creating event:', error);
@@ -96,10 +115,7 @@ const Main = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setEventData((prevState) => ({
-      ...prevState,
-      [name]: value
-    }));
+    setEventData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleSubmit = (e) => {
@@ -108,33 +124,47 @@ const Main = () => {
     setNewEventActive(false);
   };
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+
+
   return (
-    <div className='w-screen h-auto scroll-x-hidden bg-slate-50 m-0 p-0 roboto-condensed'>
-      <div className='fixed flex justify-between w-full px-8 py-4 font-bold font-sans shadow-xl bg-white z-30'>
-        <div className='text-[40px]'>Jakub</div>
-        <div className='text-[30px] flex gap-2 text-center self-center cursor-pointer hover:drop-shadow-xl transition-all duration-[100ms]' onClick={() => { setNewEventActive(!newEventActive); }}>New Event <FaPlus className='self-center text-blue-500' /></div>
+    <div className='w-screen scroll-x-hidden p-0'>
+      <div className='fixed flex justify-between w-full px-8 py-4 font-bold shadow-xl z-30 bg-gray-100'>
+        <div className='text-4xl text-blue-600'>{user?.name}</div>
+        <div className='text-2xl flex gap-2 text-center self-center cursor-pointer hover:text-blue-500 transition-colors duration-200' onClick={() => {scrollToTop(); setNewEventActive(!newEventActive)}}>
+          New Event <FaPlus className='self-center text-blue-500' />
+        </div>
       </div>
 
-      <div className='relative top-[120px] grid grid-cols-3 gap-16 mx-32 pb-12'>
+      <div className='relative top-[120px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-4 md:mx-32 pb-12'>
         {loading ? (
           <div className="col-span-3 text-center">
-            <div className="loader"></div> 
+            <div className="loader"></div>
           </div>
         ) : events.length > 0 ? (
           events.map((event) => (
-            <div key={event.id} className="w-[380px] mx-auto bg-white shadow-lg rounded-lg overflow-hidden border border-blue-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out cursor-pointer p-6 flex flex-col justify-between">
+            <div key={event.id} className="w-[400px] mx-auto bg-white shadow-lg rounded-lg overflow-hidden border border-blue-200 hover:shadow-2xl transition-shadow duration-300 ease-in-out cursor-pointer p-6 flex flex-col justify-between">
               <div>
-                <img className="w-full h-48 object-cover" src="#" alt="Event" />
+                <img className="w-full h-48 object-cover" src={event.photoUrl} alt="Event" />
               </div>
               <div>
-                <h2 className="text-4xl font-semibold text-gray-800 mb-2">{event.name}</h2>
+                <h2 className="text-2xl font-semibold text-gray-800 mb-2">{event.name}</h2>
                 <p className="text-gray-600 mb-4">{event.description}</p>
                 <div className="flex justify-between text-gray-500 text-sm mb-4">
                   <span><strong>Date:</strong> {event.date}</span>
                   <span><strong>Location:</strong> {event.location}</span>
                 </div>
-                <button className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-600 transition-colors">
-                  Take part in!
+                <button
+                  onClick={() => toggleParticipation(event.id)}
+                  className={`w-full ${user && user.interestedInEvents?.includes(event.id) ? 'bg-green-600' : 'bg-blue-600'} text-white font-semibold py-2 rounded-md hover:${user && user.interestedInEvents?.includes(event.id) ? 'bg-green-700' : 'bg-blue-700'} transition-colors`}
+                >
+                  {user && user.interestedInEvents?.includes(event.id) ? 'Drop Event' : 'Participate!'}
                 </button>
               </div>
             </div>
@@ -145,67 +175,43 @@ const Main = () => {
       </div>
 
       {newEventActive && 
-        <div className="absolute top-0 left-0 bg-white bg-opacity-[0.3] w-screen h-screen z-40 text-center" onClick={() => { setNewEventActive(!newEventActive); }}>
-          <form onClick={(e) => e.stopPropagation()} onSubmit={handleSubmit} className="absolute top-[100px] right-[50px] w-[380px] bg-white shadow-xl rounded-lg p-8 z-50">
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="name">Event Name</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={eventData.name}
-                onChange={handleChange}
-                placeholder="Enter event name"
-                className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="description">Description</label>
-              <textarea
-                id="description"
-                name="description"
-                value={eventData.description}
-                onChange={handleChange}
-                placeholder="Enter event description"
-                className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                rows="4"
-                required
-              ></textarea>
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="location">Location</label>
-              <input
-                type="text"
-                id="location"
-                name="location"
-                value={eventData.location}
-                onChange={handleChange}
-                placeholder="Enter event location"
-                className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-
-            <div className="mb-6">
-              <label className="block text-gray-700 font-bold mb-2" htmlFor="date">Event Date</label>
-              <input
-                type="date"
-                id="date"
-                name="date"
-                value={eventData.date}
-                onChange={handleChange}
-                className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                required
-              />
-            </div>
-
+        <div className="absolute top-0 left-0 bg-white bg-opacity-30 w-screen h-screen z-40 text-center" onClick={() => setNewEventActive(!newEventActive)}>
+          <form onClick={(e) => e.stopPropagation()} onSubmit={handleSubmit} className="absolute top-20 right-4 md:right-20 lg:right-32 w-full md:w-96 bg-white shadow-xl rounded-lg p-8 z-50">
+            <h2 className="text-2xl font-bold text-gray-700 mb-6">Create New Event</h2>
+            {['name', 'description', 'location', 'date'].map((field, index) => (
+              <div key={index} className="mb-4">
+                <label className="block text-gray-700 font-bold mb-2" htmlFor={field}>
+                  {field.charAt(0).toUpperCase() + field.slice(1)}
+                </label>
+                {field !== 'description' ? (
+                  <input
+                    type={field === 'date' ? 'date' : 'text'}
+                    id={field}
+                    name={field}
+                    value={eventData[field]}
+                    onChange={handleChange}
+                    placeholder={`Enter event ${field}`}
+                    className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    required
+                  />
+                ) : (
+                  <textarea
+                    id={field}
+                    name={field}
+                    value={eventData[field]}
+                    onChange={handleChange}
+                    placeholder={`Enter event ${field}`}
+                    className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    rows="4"
+                    required
+                  ></textarea>
+                )}
+              </div>
+            ))}
             <div>
               <button
                 type="submit"
-                className="w-full bg-blue-500 text-white font-bold py-3 px-4 rounded-lg hover:bg-blue-600 transition-colors"
+                className="w-full bg-blue-600 text-white font-bold py-3 px-4 rounded-lg hover:bg-blue-700 transition-colors"
               >
                 Submit Event
               </button>
